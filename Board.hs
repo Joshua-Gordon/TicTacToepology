d26fb3c113df06e849b3747c9e4054f8de012779
Josh Gordon <joshua.sam.gordon@gmail.com>
2018-06-16 14:27:30 -0400

    Add printing and moving features Added three functions in Board. emptyBoard, which takes a parameter for size and produces a square board. printBoard, which pretty prints a board. move, which takes a board, a move, and a location, and produces a new board with the updated state.

--- Board.hs
+++ Board.hs
@@ -1,9 +1,29 @@
 module Board where
 
-data Move = X | O
-  deriving (Eq, Show)
+data Move = X | O | Empty
+  deriving (Eq)
+
+instance Show Move where
+  show X = "X"
+  show O = "O"
+  show Empty = " "
 
 
 type Board = [[Move]]
 
+
 type Rule = [[Move]] -> Bool
+
+
+emptyBoard :: Int -> Board
+emptyBoard n = [[Empty | _ <- [1..n]] | _ <- [1..n]]
+
+printBoard :: Board -> IO ()
+printBoard b = let board = fmap (\row -> show row ++ "\n") b
+                in putStrLn $ concat board
+
+move :: Move -> (Int,Int) -> Board -> Board
+move new (x,y) b = [b !! n | n <- [1..x]] ++ replace (b!!x) : [b !! n | n <- [x+1 .. length b - 1], n > x]
+                    where
+                      replace :: [Move] -> [Move]
+                      replace l = [l !! m | m <- [1..y]] ++ new : [l !! m | m <- [y+1..(length l)-1]]
