e3e161975ddfe8ed609e19fd86fafcdf23db7ad3
Josh Gordon <joshua.sam.gordon@gmail.com>
2018-06-16 16:19:14 -0400

    Add board generation and checking

--- Board.hs
+++ Board.hs
@@ -18,9 +18,9 @@ type Rule = [[Move]] -> Bool
 emptyBoard :: Int -> Board
 emptyBoard n = [[Empty | _ <- [1..n]] | _ <- [1..n]]
 
-printBoard :: Board -> IO ()
+printBoard :: Board -> String
 printBoard b = let board = fmap (\row -> show row ++ "\n") b
-                in putStrLn $ concat board
+                in concat board
 
 move :: Move -> (Int,Int) -> Board -> Board
 move new (x,y) b = [b !! n | n <- [0..x-1]] ++ replace (b!!x) : [b !! n | n <- [x+1 .. length b - 1], n > x]
--- Main.hs
+++ Main.hs
@@ -1,17 +1,26 @@
 import Board
 import Standard
 import Control.Monad
+import Klein
 
 main :: IO ()
-main = let x = generateAllBoards (2,2)
-           in
-           print "bbrian"--mapM print x
+main = let filt = getStalemate (4,4) (klein 4 4)
+           printable =  concat [printBoard f ++ "\n" | f <- filt]
+       in
+       do
+         print (length filt)
+         putStrLn printable
+
+getStalemate :: (Int,Int) -> Rule -> [Board]
+getStalemate (x,y) r = let boards = generateAllBoards (x,y)
+                       in filter (not . r) boards
 
 generateAllBoards :: (Int,Int) -> [Board]
 generateAllBoards (x,y) = [chunk x list | list <- getLists (x*y)]
-                          where
-                          getLists :: Int -> [[Move]] --Generates all lists of n moves
-                          getLists n = replicateM n [X,O]
 
-                          chunk :: Int -> [a] -> [[a]]
-                          chunk n l = take n l : chunk n (drop n l)
+getLists :: Int -> [[Move]] --Generates all lists of n moves
+getLists n = replicateM n [X,O]
+
+chunk :: Int -> [a] -> [[a]]
+chunk _ [] = []
+chunk n l = take n l : chunk n (drop n l)
